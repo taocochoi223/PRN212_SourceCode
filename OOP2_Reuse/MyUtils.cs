b7a40cf3ff6b6887121265f20aa20905dba01f8f
Cao Hữu Trí <caohuutritl1234@gmail.com>
2025-05-29 19:26:31 +0700

    Initial commit - upload source code

---
+++ OOP2_Reuse/MyUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP2;
+
+namespace OOP2_Reuse
+{
+    public static class MyUtils
+    {
+        public static int TinhTuoi(this Employee employee) { 
+            return DateTime.Now.Year - employee.Birthday.Year + 1;
+        }
+    }
+}
